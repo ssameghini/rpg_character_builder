103885ab2e202130adc755651223545884272e4d
ssameghini <s.sameghini@gmail.com>
2023-11-04 21:52:32 -0300

    [FEAT] Add Character and Modifiers types

---
+++ src/types/character.ts
@@ -0,0 +1,58 @@
+export enum Ability {
+  Strength = 'Strength',
+  Dexterity = 'Dexterity',
+  Constitution = 'Constitution',
+  Intelligence = 'Intelligence',
+  Wisdom = 'Wisdom',
+  Charisma = 'Charisma',
+}
+
+export enum Skill {
+  Acrobatics = 'Acrobatics',
+  Apraise = 'Apraise',
+  Bluff = 'Bluff',
+  Climb = 'Climb',
+  Craft = 'Craft', // Might type it as an enum itself
+  Diplomacy = 'Diplomacy',
+  DisableDevice = 'Disable Device',
+  Disguise = 'Disguise',
+  EscapeArtist = 'Escape Artist',
+  Fly = 'Fly',
+  HandleAnimal = 'Handle Animal',
+  Heal = 'Heal',
+  Intimidate = 'Intimidate',
+  KnowledgeArcana = 'Knowledge (Arcana)',
+  KnowledgeDungeoneering = 'Knowledge (Dungeoneering)',
+  KnowledgeEngineering = 'Knowledge (Engineering)',
+  KnowledgeGeography = 'Knowledge (Geography)',
+  KnowledgeHistory = 'Knowledge (History)',
+  KnowledgeLocal = 'Knowledge (Local)',
+  KnowledgeNature = 'Knowledge (Nature)',
+  KnowledgeNobility = 'Knowledge (Nobility)',
+  KnowledgePlanes = 'Knowledge (Planes)',
+  KnowledgeReligion = 'Knowledge (Religion)',
+  Linguistics = 'Linguistics',
+  Perception = 'Perception',
+  Perform = 'Perform', // Might type it as an enum itself
+  Profession = 'Profession', // Might type it as an enum itself
+  Ride = 'Ride',
+  SenseMotive = 'Sense Motive',
+  SleightOfHand = 'Sleight of Hand',
+  Spellcraft = 'Spellcraft',
+  Stealth = 'Stealth',
+  Survival = 'Survival',
+  Swim = 'Swim',
+  UseMagicDevice = 'Use Magic Device',
+}
+
+export enum Size {
+  Fine = 'Fine',
+  Diminutive = 'Diminutive',
+  Tiny = 'Tiny',
+  Small = 'Small',
+  Medium = 'Medium',
+  Large = 'Large',
+  Huge = 'Huge',
+  Gargantuan = 'Gargantuan',
+  Colossal = 'Colossal',
+}
---
+++ src/types/modifiers.ts
@@ -0,0 +1,3 @@
+import { Ability } from 'types/character';
+
+export type AbilityModifier = Record<Ability, number>; // Could be better as an array?
