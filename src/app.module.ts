1d76f013ef9d552704b512ed09c22a478710bd70
ssameghini <s.sameghini@gmail.com>
2023-11-04 21:39:20 -0300

    [FEAT] Add Mongoose connection and Config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RacesModule } from './races/races.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_HOST'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
+    }),
+    RacesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
