52543114571fa91f27646d6690a5ad2e43f1798b
ssameghini <s.sameghini@gmail.com>
2024-02-13 17:05:02 -0300

    Implement in-memory database for tests

--- src/races/races.controller.spec.ts
+++ src/races/races.controller.spec.ts
@@ -1,30 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { getModelToken } from '@nestjs/mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { Model, connect, Connection } from 'mongoose';
 import { RacesController } from './races.controller';
 import { RacesService } from './races.service';
 import { RaceSchema, Race } from './schemas/race.schema';
 
 describe('RacesController', () => {
   let controller: RacesController;
+  let mongod: MongoMemoryServer;
+  let mongoConnection: Connection;
+  let raceModel: Model<Race>;
 
   beforeEach(async () => {
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    mongoConnection = (await connect(uri)).connection;
+    raceModel = mongoConnection.model(Race.name, RaceSchema);
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RacesController],
-      providers: [RacesService],
-      imports: [
-        MongooseModule.forRootAsync({
-          imports: [ConfigModule],
-          useFactory: async (configService: ConfigService) => ({
-            uri: configService.get<string>('DB_HOST'),
-          }),
-          inject: [ConfigService],
-        }),
-        ConfigModule.forRoot({
-          envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
-          isGlobal: true,
-        }),
-        MongooseModule.forFeature([{ name: Race.name, schema: RaceSchema }]),
+      providers: [
+        RacesService,
+        { provide: getModelToken(Race.name), useValue: raceModel },
       ],
     }).compile();
 
--- src/races/races.service.spec.ts
+++ src/races/races.service.spec.ts
@@ -1,28 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { Model, connect, Connection } from 'mongoose';
 import { RacesService } from './races.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { Race, RaceSchema } from './schemas/race.schema';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 describe('RacesService', () => {
   let service: RacesService;
+  let mongod: MongoMemoryServer;
+  let mongoConnection: Connection;
+  let raceModel: Model<Race>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    mongoConnection = (await connect(uri)).connection;
+    raceModel = mongoConnection.model(Race.name, RaceSchema);
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RacesService],
-      imports: [
-        MongooseModule.forRootAsync({
-          imports: [ConfigModule],
-          useFactory: async (configService: ConfigService) => ({
-            uri: configService.get<string>('DB_HOST'),
-          }),
-          inject: [ConfigService],
-        }),
-        ConfigModule.forRoot({
-          envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
-          isGlobal: true,
-        }),
-        MongooseModule.forFeature([{ name: Race.name, schema: RaceSchema }]),
+      providers: [
+        RacesService,
+        { provide: getModelToken(Race.name), useValue: raceModel },
       ],
     }).compile();
 
